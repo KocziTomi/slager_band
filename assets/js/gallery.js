36303829e1b5a250759bb9c4e7b1ad3f4b6504d0
tamas.kocziany <tamas.kocziany@gmail.com>
2023-03-05 23:18:56 +0100

    fix gallery

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,24 +1,26 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    margin: 10,
-    nav: true,
-    dots: false,
-    responsive: {
-        0: {
-            items: 1,
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        margin: 10,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            1000: {
+                items: 5,
+            },
         },
-        600: {
-            items: 3,
-        },
-        1000: {
-            items: 5,
-        },
-    },
-    navText: [
-        '<span class="fa fa-chevron-left fa-2x"></span>',
-        '<span class="fa fa-chevron-right fa-2x"></span>',
-    ],
+        navText: [
+            '<span class="fa fa-chevron-left fa-2x"></span>',
+            '<span class="fa fa-chevron-right fa-2x"></span>',
+        ],
+    })
 })
